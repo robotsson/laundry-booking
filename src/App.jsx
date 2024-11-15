c4273ae72cf451914f6c290c0eb4c1142b51f189
game-baked-jogging <game-baked-jogging@duck.com>
2024-11-15 21:21:01 +0100

    removed dummy list

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,17 @@ import DayView from './components/DayView';
 import LogIn from './components/LogIn';
 import { useState } from 'react';
 
-// 28 days (4 weeks) in json format
-import bookings from './dummybookings.jsx';
-
 function App() {
   const [data ] = useState(bookings);
 
-  // console.log(`App bookings.length: ${bookings.length}`); // sanity check that dummy bookings was loaded
-
   return (
     <BookingProvider>
-      <Router future={{v7_startTransition: true, v7_relativeSplatPath: true}}>
+      <Router future={{v7_startTransition: true, v7_relativeSplatPath:}}>
         <Routes> 
           {/* change this to have a different start page */}
           <Route path="/" element={<DayView />} />
 
-          <Route path="/calendar" element={<CalendarView bookings={data} />} /> 
+          <Route path="/calendar" element={<CalendarView />} /> 
           <Route path="/day" element={<DayView />}  /> 
           <Route path="/login" element={<LogIn />} />  
         </Routes>
