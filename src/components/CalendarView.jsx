96ba9b726226e97cf358c7c7723fd34a36a6286d
game-baked-jogging <game-baked-jogging@duck.com>
2024-11-15 21:57:30 +0100

    Show weekdays

--- src/components/CalendarView.jsx
+++ src/components/CalendarView.jsx
@@ -9,12 +9,10 @@ export default function CalendarView({bookings}) {
   const getDate = () => dayjs().format('dddd DD MMMM YYYY H:mm:ss');
 
   dayjs.extend(weekday);
-  console.log( dayjs().format('YYYY-MM-DD') );
- 
-  console.log( dayjs().locale('sv').weekday() );
-
+  // console.log( dayjs().format('YYYY-MM-DD') );
+  // console.log( dayjs().locale('sv').weekday() );
   const startDate = dayjs().locale('sv').startOf('week');
-  console.log( startDate );
+  // console.log( startDate );
   
   const days = [];
   for( let index = 0; index < 28; index++ )
@@ -27,6 +25,15 @@ export default function CalendarView({bookings}) {
     <div className="calendar">
       <h1>{getDate()}</h1>
       <div>
+        <div className="calendar-weekdays">
+          <div>Monday</div>
+          <div>Tuesday</div>
+          <div>Wednesday</div>
+          <div>Thursday</div>
+          <div>Friday</div>
+          <div>Saturday</div>
+          <div>Sunday</div>                    
+        </div>
         <div className="calendar-month">
           {days}
         </div>
