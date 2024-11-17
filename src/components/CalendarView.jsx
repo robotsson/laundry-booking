378f67d7dddd2dca286138f9831c74100bdadf98
game-baked-jogging <game-baked-jogging@duck.com>
2024-11-17 14:50:50 +0100

    Added database query

--- src/components/CalendarView.jsx
+++ src/components/CalendarView.jsx
@@ -3,8 +3,50 @@ import CalendarDay from './CalendarDay';
 import dayjs from 'dayjs';
 import 'dayjs/locale/sv';
 import weekday from 'dayjs/plugin/weekday';
+import { useEffect, useState } from 'react';
+import { supabase } from '../utils/supabase';
 
 export default function CalendarView() {  
+
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {    
+    const fetchAvailableSlots = async () => {
+      
+      try {
+        // Fetch room schedule data with relationships to Dates and Rooms tables
+        const { data, error } = await supabase        
+        .from('Room_Schedule')
+        .select(`
+          time_block,
+          owner,
+          Rooms!inner(room_name),
+          Dates!inner(date)
+          `)  
+        .gte('Dates.date', '2024-11-10') // Start date
+        .lte('Dates.date', '2024-11-17') // End date //Filter by day
+        .in('Rooms.room_name', ['room1', 'room2']); //All rooms        
+      
+      if(error) {
+        console.error(error);
+        throw error;
+      }
+      console.log(data);
+      
+
+      } catch(err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchAvailableSlots();
+  }, []); // Re-run whenever timeBlock or date changes
+
+  if (loading) return <p>Loading ...</p>;
+  if (error) return <p style={{ color: 'red' }}>{error}</p>;   
 
   const getDate = () => dayjs().format('dddd DD MMMM YYYY H:mm:ss');
 
