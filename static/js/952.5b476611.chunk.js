7c99b4644f323054ef16fb38539bc1e4c7db1c45
github.com/robotsson <game-baked-jogging@duck.com>
2024-11-21 12:38:35 +0100

    Updates

---
+++ static/js/952.5b476611.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklaundry_booking=self.webpackChunklaundry_booking||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.5b476611.chunk.js.map
