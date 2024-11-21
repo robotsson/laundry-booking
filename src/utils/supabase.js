43e19df588916c743366a26257737205e3c691b4
github.com/robotsson <game-baked-jogging@duck.com>
2024-11-21 13:03:24 +0100

    removed keys

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,9 +1,9 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrlpt = 'https://dtpatbzxwqzmwhmrdinf.supabase.co';
-const supabaseKeypt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0cGF0Ynp4d3F6bXdobXJkaW5mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIxMTQ2NTQsImV4cCI6MjA0NzY5MDY1NH0.XesACcib0m8omZVFVUWSNhMJ8MytFkjsyo8JLLc24w4';
+const supabaseUrl = 'https://dtpatbzxwqzmwhmrdinf.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0cGF0Ynp4d3F6bXdobXJkaW5mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIxMTQ2NTQsImV4cCI6MjA0NzY5MDY1NH0.XesACcib0m8omZVFVUWSNhMJ8MytFkjsyo8JLLc24w4';
 
 
-export const supabase = createClient(supabaseUrlpt, supabaseKeypt);
+export const supabase = createClient(supabaseUrl, supabaseKey);
         
