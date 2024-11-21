6a92ae5b2a092e34a7a685063d1732dc6327b45a
github.com/robotsson <game-baked-jogging@duck.com>
2024-11-21 12:14:18 +0100

    uncommented unused keys

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -2,8 +2,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Es
-const supabaseUrl = 'https://fuwpynywldgbvoycjfqx.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1d3B5bnl3bGRnYnZveWNqZnF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA5ODc3MTUsImV4cCI6MjA0NjU2MzcxNX0.c5J9CLU1kZF9l_Uc50Fejatqly6aKIMj6TCvT6f6NPg';
+// const supabaseUrl = 'https://fuwpynywldgbvoycjfqx.supabase.co';
+// const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1d3B5bnl3bGRnYnZveWNqZnF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA5ODc3MTUsImV4cCI6MjA0NjU2MzcxNX0.c5J9CLU1kZF9l_Uc50Fejatqly6aKIMj6TCvT6f6NPg';
 
 // My stuff
 const supabaseUrlpt = 'https://dtpatbzxwqzmwhmrdinf.supabase.co';
